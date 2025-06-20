f5837dc712f5653a0a976afb6fca95b5a52a799e
PaoloOliviero <paolo.oliviero3@gmail.com>
2025-06-20 16:16:03 +0200

    Commit Inizio Scanner

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        Immagine img = new Immagine ("Paesaggio", 5);
-        RegistrazioneAudio ra = new RegistrazioneAudio ("Podcast", 4, 3);
-        Video deo = new Video ("harry", 3, 5, 45);
+        Immagine img = new Immagine("Paesaggio", 5);
+        RegistrazioneAudio ra = new RegistrazioneAudio("Podcast", 4, 3);
+        Video deo = new Video("harry", 3, 5, 45);
 
         System.out.println();
         img.show();
@@ -18,6 +18,7 @@ public class Main {
         deo.Riproduzione();
 
         ClasseAstrattaElemento[] Elementi = new ClasseAstrattaElemento[5];
+    }
 
 
 
